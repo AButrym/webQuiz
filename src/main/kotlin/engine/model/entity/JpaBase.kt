06284cd26958d347336fafad2faec98ffe523ae4
obutr <butrym@karazin.ua>
2025-08-26 13:14:13 +0300

    Small refactoring in JpaBase.kt

--- src/main/kotlin/engine/model/entity/JpaBase.kt
+++ src/main/kotlin/engine/model/entity/JpaBase.kt
@@ -9,15 +9,16 @@ abstract class JpaBase {
         o.hibernateLazyInitializer.persistentClass
     else o.javaClass
 
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null) return false
-        if (getClass(o) != getClass(this)) return false
-        val other: JpaBase = o as JpaBase
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+        if (getClass(other) != getClass(this)) return false
+        val other: JpaBase = other as JpaBase
         return id != null && id == other.id
     }
 
     override fun hashCode(): Int = getClass(this).hashCode()
 
-    override fun toString(): String = "${getClass(this).simpleName}[id=$id]"
+    override fun toString(): String =
+        "${getClass(this).simpleName.replace("Entity","")}[id=$id]"
 }
